aae2aadb161d87a53e32b3c334e1a0c98d0c902c
@Atemkengfranck2 <atemkengfalcao2@gmail.com>
2024-12-15 18:30:13 +0100

    integration des action pour la gestion de la database

--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -1,18 +1,22 @@
+import os
 from fastapi.testclient import TestClient
 from api.app import app
-from api.models.user import User  # Assurez-vous d'importer votre modèle User
-from api.database import SessionLocal, engine, Base  # Importez la base de données et le moteur
+from api.models.user import User  # Ensure you import your User model
+from api.database import SessionLocal, engine, Base  # Import the database and engine
+
+# Set the environment variable to 'test'
+os.environ["ENVIRONMENT"] = "test"
 
 client = TestClient(app)
 
-# Utilitaire pour nettoyer la base de données
+# Utility to clear the users table
 def clear_users():
     db = SessionLocal()
-    db.query(User).delete()  # Efface tous les utilisateurs pour éviter les conflits d'email
+    db.query(User).delete()  # Clear all users to avoid email conflicts
     db.commit()
     db.close()
 
-# Avant chaque test, réinitialisez la base de données
+# Before each test, reset the database
 def setup_db():
     Base.metadata.create_all(bind=engine)
 
@@ -20,46 +24,3 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Bienvenue sur la Shop App API"}
-
-def test_get_products():
-    response = client.get("/api/v1/produits/")
-    assert response.status_code == 200
-
-def test_create_product():
-    response = client.post(
-        "/api/v1/produits/",
-        json={
-            "name": "Test Product",
-            "price": 25.0,
-            "description": "Test description",
-            "in_stock": True,
-        },
-    )
-    assert response.status_code == 200
-    assert response.json()["name"] == "Test Product"
-
-def test_register_user():
-    clear_users()  # Nettoie la base de données avant d'exécuter le test
-    
-    response = client.post(
-        "/api/v1/users/register",
-        json={
-            "username": "newuser",
-            "email": "user@example.com",  # Changez d'email si nécessaire pour éviter les conflits
-            "password": "password123",
-        },
-    )
-    assert response.status_code == 200
-    assert response.json()["message"] == "Utilisateur créé avec succès"
-
-def test_login_user():
-    # S'assurer qu'un utilisateur existe avant de tester la connexion
-    response = client.post(
-        "/api/v1/users/login",
-        json={
-            "username": "newuser",
-            "password": "password123",
-        },
-    )
-    assert response.status_code == 200
-    assert response.json()["message"] == "Connexion réussie"
