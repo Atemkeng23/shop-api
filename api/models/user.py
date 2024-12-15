b7381b9f8c76604c311b53b32a34281a321865d5
@Atemkengfranck2 <atemkengfalcao2@gmail.com>
2024-12-15 18:38:40 +0100

    update des User pour s'assurer que les types de données soient supporter

--- api/models/user.py
+++ api/models/user.py
@@ -5,6 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
+    username = Column(String(50), unique=True, index=True)  # Ensure the type is supported for indexing
+    email = Column(String(255), unique=True, index=True)  # Change to VARCHAR(255) to ensure it can be indexed
     hashed_password = Column(String)
