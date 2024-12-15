454c068ed077b45315d0739347e75d23b08fb7f5
@Atemkengfranck2 <atemkengfalcao2@gmail.com>
2024-12-15 18:27:53 +0100

    configuration de la base de donnée avec sql-server sur azure

--- api/database.py
+++ api/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from urllib.parse import quote_plus
 import os
 import logging
@@ -8,33 +7,32 @@ import logging
 logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
-# Détermine si on est en mode test ou production
-is_test = os.getenv("ENVIRONMENT") == "test"  # Vous pouvez définir cette variable d'environnement pour les tests
+# Determine if we are in test or production mode
+is_test = os.getenv("ENVIRONMENT") == "test"  # You can set this environment variable for tests
 
-# Paramètres de la base de données SQL Server (production)
+# Database settings for SQL Server (production)
 password = "P@ssword123!"
 encoded_password = quote_plus(password)
 SQLALCHEMY_DATABASE_URL = (
-    f"mssql+pyodbc://adminuser:{encoded_password}@shop-sql-server-mm5ysr.database.windows.net:1433/shop-database?driver=ODBC+Driver+17+for+SQL+Server"
+    f"mssql+pyodbc://adminuser:{encoded_password}@shop-sql-server-api.database.windows.net:1433/shop-database?driver=ODBC+Driver+17+for+SQL+Server"
 )
 
-# Paramètres de la base de données SQLite (test)
-SQLALCHEMY_DATABASE_URL_TEST = "sqlite:///:memory:"  # SQLite en mémoire pour les tests
+# Database settings for SQLite (test)
+SQLALCHEMY_DATABASE_URL_TEST = "sqlite:///:memory:"  # SQLite in-memory for tests
 
-# Choisir l'URL de la base de données selon l'environnement
+# Choose the database URL based on the environment
 DATABASE_URL = SQLALCHEMY_DATABASE_URL_TEST if is_test else SQLALCHEMY_DATABASE_URL
 
-# Créer l'engine SQLAlchemy
+# Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if is_test else {})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Fonction pour obtenir la session de la base de données
+# Function to get the database session
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
